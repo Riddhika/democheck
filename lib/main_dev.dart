529ea304044417a6974bcece01b20a254b53123d
Pinal Dhaduk <pinal.dhaduk@sensussoft.com>
2022-07-16 10:17:28 +0530

    dev file is updated

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -4,4 +4,6 @@ import 'package:flutter_mvvm_structure/variant/app_config.dart';
 void main(){
   ConstantEnvironment.setEnvironment(Environment.dev);
   mainDelegate();
+
+  // main dev file is updated
 }
