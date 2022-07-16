40ffeb6602c2d455573db7b194feed5a96bf4536
Pinal Dhaduk <pinal.dhaduk@sensussoft.com>
2022-07-16 09:35:50 +0530

    login moudule done

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -4,4 +4,6 @@ import 'package:flutter_mvvm_structure/variant/app_config.dart';
 void main(){
   ConstantEnvironment.setEnvironment(Environment.dev);
   mainDelegate();
+
+  // pinal add login module
 }
