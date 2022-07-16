9633cf0a9a28ac608011c66d247b66011e8d0e18
pinalsensussoft <pinal.dhaduk@sensussoft.com>
2022-07-16 10:52:25 +0530

    Revert "PR TASK/ADB-1-Add login module"

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -4,6 +4,4 @@ import 'package:flutter_mvvm_structure/variant/app_config.dart';
 void main(){
   ConstantEnvironment.setEnvironment(Environment.dev);
   mainDelegate();
-
-  // pinal add login module
 }
